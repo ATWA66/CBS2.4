0bef5ce08f75ef02f8d309c2b50e920b0aa107cc
Denis Tokmenko <98480275+ATWA66@users.noreply.github.com>
2022-09-11 15:08:38 +0300

    Add files via upload

---
+++ CBS4/Program.cs
@@ -0,0 +1,68 @@
+﻿XMLHandler XML = new XMLHandler();
+TXTHandler TXT = new TXTHandler();
+DOCHandler DOC = new DOCHandler();
+XML.Create();
+TXT.Create();
+DOC.Create();
+abstract class AbstractHandler {
+    public void Open() { }
+    public void Create() { }
+    public void Change() { }
+    public void Save() { }
+}
+class XMLHandler : AbstractHandler
+{
+    public void open() {
+        Console.WriteLine("open in XML");
+    }
+    public void Create()
+    {
+        Console.WriteLine("Create in XML");
+    }
+    public void Change()
+    {
+        Console.WriteLine("Change in XML");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Save in XML");
+    }
+}
+class TXTHandler : AbstractHandler
+{
+    public void open()
+    {
+        Console.WriteLine("open in TXT");
+    }
+    public void Create()
+    {
+        Console.WriteLine("Create in TXT");
+    }
+    public void Change()
+    {
+        Console.WriteLine("Change in TXT");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Save in TXT");
+    }
+}
+class DOCHandler : AbstractHandler
+{
+    public void open()
+    {
+        Console.WriteLine("open in DOC");
+    }
+    public void Create()
+    {
+        Console.WriteLine("Create in DOC");
+    }
+    public void Change()
+    {
+        Console.WriteLine("Change in DOC");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Save in DOC");
+    }
+}
---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,32 @@
+﻿Player instance = new Player();
+instance.Play();
+instance.Pause();
+instance.Stop();
+instance.Record();
+interface IPlayable
+{
+    void Play() { }
+    void Pause() { }
+    void Stop() { }
+}
+interface IRecodable
+{
+    void Record() { }
+    void Pause() { }
+    void Stop() { }
+}
+class Player : IPlayable, IRecodable
+{
+   public void Play() {
+        Console.WriteLine("play...");
+    }
+    public void Pause() {
+        Console.WriteLine("pause record/playing");
+    } 
+    public void Stop() {
+        Console.WriteLine("Stop playing/record");
+    }
+    public void Record() {
+        Console.WriteLine("record...");
+    }
+}
